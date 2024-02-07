2e57bbf3d49a7606e1323f5aa34e1caeecf7de1a
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-02-07 20:13:28 +0700

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const responseHandler = require("../helpers/responseHandler");
 const auth = require("./auth");
+const party = require("./party");
 
 router.get("/", (req, res) => {
   return responseHandler.succes(res, "Test Route");
 });
 
 router.use("/auth", auth);
+router.use("/party", party);
 
 module.exports = router;
