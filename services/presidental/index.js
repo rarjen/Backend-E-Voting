4a5fd16d0ab06e93d59e622c9e080a34fa32b1b2
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-02-07 21:26:51 +0700

    feat: create services/presidental/index.js

---
+++ services/presidental/index.js
@@ -0,0 +1,63 @@
+const { PrismaClient } = require("@prisma/client");
+const ApiError = require("../../helpers/errorHandler");
+const { presidental_candidate, political_party } = new PrismaClient();
+
+const create = async (req) => {
+  const partyExist = await political_party.findFirst({
+    where: { id: req.body.political_party_id },
+  });
+
+  if (!partyExist) {
+    throw ApiError.notFound("Data tidak ada!");
+  }
+
+  const result = await presidental_candidate.create({ data: req.body });
+
+  return result;
+};
+
+const getOne = async (req) => {
+  const result = await presidental_candidate.findFirst({
+    where: { id: req.params.id },
+    include: { political_party: true },
+  });
+
+  if (!result) {
+    throw ApiError.notFound("Data tidak ada!");
+  }
+
+  return result;
+};
+
+const getAll = async (req) => {
+  const result = await presidental_candidate.findMany({
+    include: { political_party: true },
+  });
+
+  return result;
+};
+
+const update = async (req) => {
+  getOne(req);
+
+  const result = await presidental_candidate.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+
+  return result;
+};
+
+const destroy = async (req) => {
+  getOne(req);
+
+  const result = await presidental_candidate.delete({
+    where: { id: req.params.id },
+  });
+
+  return result;
+};
+
+module.exports = { create, getOne, update, getAll, destroy };
