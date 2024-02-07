a856992bdcd2d72cfb24d6df802d5329ba85c6c6
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-02-07 09:55:26 +0700

    update errorHandler.js

---
+++ helpers/errorHandler.js
@@ -0,0 +1,43 @@
+const { StatusCodes } = require("http-status-codes");
+const { constants } = require("http2");
+
+class ApiError {
+  constructor(code, message, errors = {}) {
+    this.code = code;
+    this.message = message;
+    if (errors) {
+      this.errors = errors;
+    }
+  }
+
+  static badRequest(message) {
+    return new ApiError(constants.HTTP_STATUS_BAD_REQUEST, message);
+  }
+
+  static unauthorized(
+    message = StatusCodes[constants.HTTP_STATUS_UNAUTHORIZED]
+  ) {
+    return new ApiError(constants.HTTP_STATUS_UNAUTHORIZED, message);
+  }
+
+  static forbidden(message = StatusCodes[constants.HTTP_STATUS_FORBIDDEN]) {
+    return new ApiError(constants.HTTP_STATUS_FORBIDDEN, message);
+  }
+
+  static notFound(message = StatusCodes[constants.HTTP_STATUS_NOT_FOUND]) {
+    return new ApiError(constants.HTTP_STATUS_NOT_FOUND, message);
+  }
+
+  static unprocessableEntity(
+    message = StatusCodes[constants.HTTP_STATUS_UNPROCESSABLE_ENTITY],
+    errors = {}
+  ) {
+    return new ApiError(
+      constants.HTTP_STATUS_UNPROCESSABLE_ENTITY,
+      message,
+      errors
+    );
+  }
+}
+
+module.exports = ApiError;
