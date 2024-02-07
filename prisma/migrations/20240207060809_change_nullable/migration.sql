9189fe4b5a702fa5b56df2929eac71dcdbb58adb
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-02-07 13:08:46 +0700

    feat: create migration.sql and migration.sql

---
+++ prisma/migrations/20240207060809_change_nullable/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `isVoted` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isVoted" BOOLEAN NOT NULL,
+ALTER COLUMN "address" DROP NOT NULL;
